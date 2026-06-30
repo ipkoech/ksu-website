728739d6861bfb538d3862ec80a06b68171f70ce
rkyegon <rkyegon@kisiiuniversity.ac.ke>
2026-06-30 08:27:10 +0300

    Merge research migration heads

---
+++ services/research/migrations/versions/20260630_0006_merge_research_heads.py
@@ -0,0 +1,22 @@
+"""merge research migration heads
+
+Revision ID: 20260630_0006_merge_research_heads
+Revises: 20260630_0004, d05a1b2c3e4f
+Create Date: 2026-06-30 08:27:00.000000
+"""
+
+from __future__ import annotations
+
+
+revision = "20260630_0006_merge_research_heads"
+down_revision = ("20260630_0004", "d05a1b2c3e4f")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
